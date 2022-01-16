3d994b8f391905d4e82131a532f001890380b06f
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-16 14:41:32 +0530

    Add Log for every Req and Res

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,20 @@ import config from './config/default';
 
 const app = express();
 
+/**
+ * Middleware: Log every request
+ */
+
+app.use((req, res, next) => {
+	logger.info(`METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+	res.on('finish', () => {
+		logger.info(`METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`);
+	})
+
+	next();
+})
+
 app.use(express.json());
 
 app.listen(5000, () => {
