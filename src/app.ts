0d6b8b8b5a744552154cf12015ddf212c232dc1d
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-16 22:58:11 +0530

    Add rules of api middleware

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,23 @@ app.use((req, res, next) => {
 	next();
 })
 
+/**
+ * Rules of the API
+ */
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+	if(req.method == 'OPTIONS'){
+		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+		return res.status(200).json({});
+	}
+	next();
+})
+
+
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
 	res.send("/ endpoint hit");
