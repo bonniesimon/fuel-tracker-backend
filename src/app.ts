3055b280639c4b1a1ff25754631a02f1246fec66
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-16 14:38:01 +0530

    Add config file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import logger from "./utils/logger";
+import config from './config/default';
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(5000, () => {
-	logger.info(`App is running at `);
+	logger.info(`App is running at ${config.server.hostname}:${config.server.port}`);
 })
