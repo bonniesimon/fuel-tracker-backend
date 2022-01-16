fdb0e572573082270d9db538cd794674c698c4a9
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-16 22:58:26 +0530

    Add mongodb connections uri

--- src/config/default.ts
+++ src/config/default.ts
@@ -5,13 +5,22 @@ dotenv.config();
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 5000;
 
+const MONGODB_CLUSTER_PASSWORD = process.env.MONGODB_CLUSTER_PASSWORD;
+const MONGODB_DATABASE = process.env.MONGODB_DATABASE_NAME;
+const MONGODB_URI = `mongodb+srv://bonniesimon:${MONGODB_CLUSTER_PASSWORD}@fuel-tracker.085xe.mongodb.net/${MONGODB_DATABASE}?retryWrites=true&w=majority`
+
 const SERVER = {
 	hostname: SERVER_HOSTNAME,
 	port: SERVER_PORT
 }
 
+const DATABASE = {
+	uri: MONGODB_URI
+}
+
 const config = {
-	server: SERVER
+	server: SERVER,
+	database: DATABASE
 }
 
 
