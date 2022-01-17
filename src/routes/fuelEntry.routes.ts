e8667a7dea43e4e270142be458360fbb753ea2d1
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-17 13:19:53 +0530

    Add get all fuelentry route

--- src/routes/fuelEntry.routes.ts
+++ src/routes/fuelEntry.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createFuelEntryHandler } from '../controller/fuelEntry.controller';
+import { createFuelEntryHandler, getAllFuelEntryHandler } from '../controller/fuelEntry.controller';
 
 const router = express.Router();
 
+router.get("/api/fuelentry/all", getAllFuelEntryHandler);
 router.post("/api/fuelentry/create", createFuelEntryHandler);
 
 
