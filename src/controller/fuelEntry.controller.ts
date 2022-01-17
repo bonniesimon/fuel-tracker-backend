bb68a6eb3a647da3dfb99bd91f2f5d19b9147cb7
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-17 13:20:09 +0530

    Add controller to get all fuelentry from db

--- src/controller/fuelEntry.controller.ts
+++ src/controller/fuelEntry.controller.ts
@@ -2,10 +2,11 @@ import {Request, Response} from "express";
 import FuelEntryModel from "../model/fuelEntry.model";
 import log from "../utils/logger";
 
-
+/**
+ * TODO: Don't create document if document with same field exists 
+ */
 const createFuelEntryHandler = async (req:Request, res: Response) => {
 	const {carID, entryDate, amount, litres, pricePerLitre, kilometerReading} = req.body;
-
 
 	try{
 		const newFuelEntry = new FuelEntryModel({
@@ -23,8 +24,16 @@ const createFuelEntryHandler = async (req:Request, res: Response) => {
 		log.error(e.message);
 		return res.status(500).send("Server Error");
 	}
-
-	
 }
 
-export {createFuelEntryHandler};
+const getAllFuelEntryHandler = async (req: Request, res: Response) => {
+	try{
+		const result = await FuelEntryModel.find({});
+		return res.status(200).json(result);
+	}catch(e: any){
+		log.error(e.message);
+		return res.status(500).send("Server Error");
+	}
+}
+
+export {createFuelEntryHandler, getAllFuelEntryHandler};
