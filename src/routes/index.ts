09f7d9e8ef64892639e594d9284c0f170181c82d
Bonnie Simon <bonnie.developer@gmail.com>
2022-03-03 13:35:59 +0530

    Update / and /healthcheck routes.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,12 @@ import fuelEntry from '../routes/fuelEntry.routes';
 
 const router = express.Router();
 
+router.get("/", (_, res) => {
+	res.status(200).send("Fuel Tracker API v0.1");
+});
+
 router.get("/healthcheck", (_, res) => {
-	res.sendStatus(200);
+	res.status(200).send("Fuel Tracker API is online");
 });
 
 /**
