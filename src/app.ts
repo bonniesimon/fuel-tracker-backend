5324730894fc127b7866ef27e0952fa5b3f795bc
Bonnie Simon <bonnie.developer@gmail.com>
2022-03-03 18:22:16 +0530

    Remove forward slash in cors site list

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
  * Rules of the API
  */
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://fuel-tracker.netlify.app/']);
+	res.header('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://fuel-tracker.netlify.app']);
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	
 	if(req.method == 'OPTIONS'){
