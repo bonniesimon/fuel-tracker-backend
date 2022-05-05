46d47e62b56fd637bfeb4971de17aa7d91bfb952
Bonnie Simon <bonnie.developer@gmail.com>
2022-05-05 13:36:55 +0530

    Remove unused imports

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from "express";
+import express from "express";
 import {createCarHandler, getAllCarsHandler} from "../controller/car.controller";
 
 const router = express.Router();
